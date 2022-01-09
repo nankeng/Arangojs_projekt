fe5643390ec120dbadcdb34abe9be345ac212f65
anguet2s <anguet2s@smail.inf.h-brs.de>
2022-01-09 10:18:12 +0100

    fix header url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const db = new Database({
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://10.20.110.48:8529/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://10.20.110.61:808');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
